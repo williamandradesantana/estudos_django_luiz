54d41764c9827ad077d0b1b1645c0af5228727a3
Willian Batista Andrade Santana <4064038@banese.com.br>
2024-08-21 10:36:40 -0300

    teste

--- first_project/projeto/urls.py
+++ first_project/projeto/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),  # /
-    path('recipes/', include('recipes.urls'))  # dominio.com/recipes/
+    path('recipes/', include('recipes.urls')), # dominio.com/recipes/
 ]
