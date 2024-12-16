2d24bb6e385da7dfa9094ba65608a572acbdfdf6
William Santana <williamandrade1058@gmail.com>
2024-12-16 11:40:29 -0300

    Injetando dados na lista da view Home

--- first_project/recipes/views.py
+++ first_project/recipes/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-
+from utils.recipes import factory
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', status=201, context={'name': 'William', 'team': 'Flamengo'})
+    return render(request, 'recipes/pages/home.html', 
+        status=201, 
+        context={'recipes': [factory.make_recipe() for _ in range (10)]})
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html',)
+    return render(request, 'recipes/pages/recipe-view.html',
+        context={'recipe': factory.make_recipe()})
 
 def contact(request):
     return render(request, 'recipes//pages/contato.html', context={'contact': "(79) 99916-5932"})
