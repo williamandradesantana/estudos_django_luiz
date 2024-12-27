f000d821dc5fb6d300979663d6b83a37638dfda7
William Santana <williamandrade1058@gmail.com>
2024-12-27 11:20:54 -0300

    Renomeando um projeto Django inteiro

--- first_project/project/settings.py
+++ first_project/project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'projeto.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'projeto.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
