0797a98bbe65c6fad4739fb27f536a6aad3f3194
William Santana <williamandrade1058@gmail.com>
2024-12-29 09:41:35 -0300

    Separando os testes em um package chamado tests com init.py

--- first_project/recipes/tests/test_recipe_views.py
+++ first_project/recipes/tests/test_recipe_views.py
@@ -3,22 +3,6 @@ from django.urls import reverse, resolve
 
 from recipes import views
 
-# Create your tests here.
-
-class RecipeURLsTest(TestCase):
-    
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-    
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id' : 1})
-        self.assertEqual(url, f'/recipes/category/1/')
-
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, f'/recipes/1/')
-
 class RecipeViewsTest(TestCase):
 
     def test_recipe_home_view_function_is_correct(self):
