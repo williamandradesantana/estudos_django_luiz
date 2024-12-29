41ad9a8b709e1743d77f95835ec0ce234845ab14
William Santana <williamandrade1058@gmail.com>
2024-12-29 15:53:43 -0300

    Testando 404 Not Found quando receitas não existem

--- first_project/recipes/tests/test_recipe_views.py
+++ first_project/recipes/tests/test_recipe_views.py
@@ -9,17 +9,7 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse('recipes:home'))
         self.assertTrue(True)
         self.assertIs(view.func, views.home)
-
-    def test_recipe_category_view_function_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertTrue(True)
-        self.assertIs(view.func, views.category)
     
-    def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertTrue(True)
-        self.assertIs(view.func, views.recipe)
-
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         
@@ -36,3 +26,28 @@ class RecipeViewsTest(TestCase):
             'No recipes found here!', 
             response.content.decode('utf-8')
             )
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1000}))
+        self.assertTrue(True)
+        self.assertIs(view.func, views.category)
+    
+    def test_recipe_category_view_returns_status_code_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
+
+        self.assertEqual(response.status_code, 404)
+    
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertTrue(True)
+        self.assertIs(view.func, views.recipe)
+    
+    def test_recipe_detail_view_returns_status_code_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+
+        self.assertEqual(response.status_code, 404)
+    
