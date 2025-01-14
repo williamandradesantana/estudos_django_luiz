987dbfc6bd30376d83784028b98508a723b55f65
William Santana <williamandrade1058@gmail.com>
2025-01-13 23:19:58 -0300

    testando string representation da Recipe

--- first_project/recipes/tests/test_recipe_model_recipe.py
+++ first_project/recipes/tests/test_recipe_model_recipe.py
@@ -55,3 +55,15 @@ class RecipeModelTest(RecipeTestBase):
             recipe.is_published,
             msg='Recipe is_published is not False'
         )
+    
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+
+        self.assertEqual(
+            str(self.recipe), 
+            needed,
+            msg=f'Recipe string representation must be {needed} but it is {self.recipe.title} was received'
+            )
