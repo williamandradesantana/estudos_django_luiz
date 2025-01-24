392a208aea56309350d35412312275a8a23d60fc
William Santana <williamandrade1058@gmail.com>
2025-01-23 22:34:51 -0300

    Segurança: Cross-site scripting (XSS)

--- first_project/recipes/views.py
+++ first_project/recipes/views.py
@@ -34,12 +34,14 @@ def recipe(request, id):
         })
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
     
     if not search_term:
         raise Http404
 
-    return render(request, 'recipes/pages/search.html')
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}"', 'search_term': search_term
+    })
 
 def contact(request):
     return render(request, 'recipes/pages/contato.html', context={'contact': "(79) 99916-5932"})
