397bab11aa082e39d29cc7728780e6784d55d4e1
William Santana <williamandrade1058@gmail.com>
2025-03-03 18:34:48 -0300

    Adicionando paginação no template

--- first_project/recipes/views.py
+++ first_project/recipes/views.py
@@ -4,16 +4,30 @@ from django.db.models import Q
 from django.core.paginator import Paginator
 
 from recipes.models import Recipe
+from utils.pagination import make_pagination_range
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    current_page = request.GET.get('page', 1)
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    
     paginator = Paginator(recipes, 9)
     page_obj = paginator.get_page(current_page)
 
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page=current_page,
+    )
+
     return render(request, 'recipes/pages/home.html',
-        context={'recipes': page_obj}
+        context={
+            'recipes': page_obj,
+            'pagination_range': pagination_range
+        }
     )
 
 def category(request, category_id):
