ec78832e7e14c598afd0b2b5ead9d57e97aaaec3
William Santana <williamandrade1058@gmail.com>
2025-03-05 17:00:04 -0300

    python-dotenv - movendo PER_PAGE para variáveis de ambiente no Django (.env)

--- first_project/recipes/views.py
+++ first_project/recipes/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
 from django.db.models import Q
-from django.core.paginator import Paginator
 
 from recipes.models import Recipe
 from utils.pagination import make_pagination
 
+import os
+
+PER_PAGE = os.environ.get('PER_PAGE', 6)
+
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html',
         context={
@@ -22,7 +25,7 @@ def category(request, category_id):
     
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', 
         status=201, 
@@ -57,7 +60,7 @@ def search(request):
         is_published=True
         ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"', 
