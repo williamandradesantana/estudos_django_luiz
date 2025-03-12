1dbe2fb2f358b0619776d1ba321ed13bba44a4fe
William Santana <williamandrade1058@gmail.com>
2025-03-12 15:11:28 -0300

    Testando a paginação da home e uma breve introdução a Mocks e Patch do unittest

--- first_project/recipes/tests/test_recipe_home_view.py
+++ first_project/recipes/tests/test_recipe_home_view.py
@@ -1,4 +1,5 @@
 from django.urls import reverse, resolve
+from unittest.mock import patch
 
 from recipes import views
 from .test_recipe_base import RecipeTestBase
@@ -54,3 +55,22 @@ class RecipeHomeViewTest(RecipeTestBase):
             'No recipes found here!', 
             response.content.decode('utf-8')
             )
+        
+    # @patch('recipes.views.PER_PAGE', new=3)
+    def test_recipe_home_is_paginated(self):
+        """ Test recipe is_published=False dont show """
+        # Need a recipe for this test
+        for i in range(8):
+            kwargs = {'author_data': {'username': f'u{i}'}, 'slug': f'r{i}'}
+            self.make_recipe(**kwargs)
+        
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home'))
+
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 3)
+            self.assertEqual(len(paginator.get_page(2)), 3)
+            self.assertEqual(len(paginator.get_page(3)), 2)
