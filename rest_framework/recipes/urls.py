101fe7d2703869866d009bc5ea2722b264ceef6f
William Santana <williamandrade1058@gmail.com>
2025-08-08 14:45:48 -0300

    Utilizando um serializer para list e detail com o Django Rest Framework

--- rest_framework/recipes/urls.py
+++ rest_framework/recipes/urls.py
@@ -49,5 +49,10 @@ urlpatterns = [
         'recipes/api/v2/',
         api.recipe_api_list,
         name='recipes_api_v2'
-    )
+    ),
+    path(
+        'recipes/api/v2/<int:pk>/',
+        api.recipe_api_detail,
+        name="recipes_api_v2_details"
+    ),
 ]
--- rest_framework/recipes/views/api.py
+++ rest_framework/recipes/views/api.py
@@ -1,7 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
+
+from ..models import Recipe
+from ..serializers import RecipeSerializer
 
 @api_view(["GET"])
 def recipe_api_list(request):
-    return Response("ok")
+    recipes = Recipe.objects.all()
+    serializer = RecipeSerializer(instance=recipes, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def recipe_api_detail(request, pk):
+    recipe = get_object_or_404(
+        Recipe.objects.all(), pk=pk
+    )
+    serializer = RecipeSerializer(instance=recipe, many=False)
+    return Response(serializer.data)
+    
+    # recipe = Recipe.objects.all().filter(pk=pk).first()
+    # if recipe:
+    #     serializer = RecipeSerializer(instance=recipe, many=False)
+    #     return Response(serializer.data)
+    # else:
+    #     return Response({'detail': 'eita'}, status=status.HTTP_418_IM_A_TEAPOT)
