09461f95461c38b59116c4a7ed7e6ffcb291c21b
William Santana <williamandrade1058@gmail.com>
2025-08-10 13:20:41 -0300

    Enviado dados para o serializer no Django Rest Framework

--- rest_framework/recipes/views/api.py
+++ rest_framework/recipes/views/api.py
@@ -18,7 +18,11 @@ def recipe_api_list(request):
         return Response(serializer.data)
 
     elif request.method == "POST":
-        return Response("POST", status=status.HTTP_201_CREATED)
+        serializer = RecipeSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
 @api_view(["GET"])
 def recipe_api_detail(request, pk):
