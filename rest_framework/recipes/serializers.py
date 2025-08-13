a264e30728dc5be7bef464091898c2f925e475a0
William Santana <williamandrade1058@gmail.com>
2025-08-13 14:54:03 -0300

    Adicionando Category, Author e Tags na Recipe no método save()

--- rest_framework/recipes/serializers.py
+++ rest_framework/recipes/serializers.py
@@ -43,3 +43,12 @@ class RecipeSerializer(serializers.ModelSerializer):
         super_validate = super().validate(attrs)
         AuthorRecipeValidator(data=attrs, ErrorClass=serializers.ValidationError)
         return super_validate
+    
+    def save(self, **kwargs):
+        return super().save(**kwargs)
+    
+    def create(self, validated_data):
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
