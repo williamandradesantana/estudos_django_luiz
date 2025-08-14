027a83643caab6023db8c6bb91ac5c2505017238
William Santana <williamandrade1058@gmail.com>
2025-08-14 15:14:22 -0300

    Mixins, Generic Views e Pagination no Django Rest Framework

--- rest_framework/recipes/views/api.py
+++ rest_framework/recipes/views/api.py
@@ -3,27 +3,35 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 from tag.models import Tag
 
-class RecipeAPIv2List(APIView):
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 2
+
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
+
+    # def get(self, request):
+    #     recipes = Recipe.objects.all()
+    #     serializer = RecipeSerializer(instance=recipes, many=True, context={'request': request})
+    #     return Response(serializer.data)
     
-    def get(self, request):
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(instance=recipes, many=True, context={'request': request})
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = RecipeSerializer(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save(
-            author_id=1, category_id=1,
-            tags=[1,2]
-        )
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # def post(self, request):
+    #     serializer = RecipeSerializer(data=request.data, context={'request': request})
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save(
+    #         author_id=1, category_id=1,
+    #         tags=[1,2]
+    #     )
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class RecipeAPIv2Detail(APIView):
